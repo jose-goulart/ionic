9e0612a023407b2ca01148e36d68ad8c0e05ae5e
jose-goulart <josevictor2000goulart@hotmail.com>
2022-08-13 14:01:49 -0300

    Redirect to page memories and use base-layout

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <base-layout page-title="All Memories">
+    <ion-list>
+      <ion-item router-link="/memories/1">A trip into the montains</ion-item>
+      <ion-item>Surfing the sea side</ion-item>
+      <ion-item>Good eating</ion-item>
+    </ion-list>
+  </base-layout>
+</template>
+
+<script>
+
+import { 
+  IonList,
+  IonItem
+} from '@ionic/vue';
+
+export default {
+  components: {
+    IonList,
+    IonItem
+  }
+}
+</script>
